c2959bfa591498f9d4c87739639d26aa66111e9a
Ricardo B. Ayres <ricardo.bosqueiro.ayres@gmail.com>
2022-10-18 00:05:02 -0300

    set port from environment

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	_ "github.com/glebarez/go-sqlite"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 )
@@ -106,8 +107,14 @@ func main() {
 	http.Handle("/", http.FileServer(http.Dir("./static")))
 	http.HandleFunc("/users/", Users(db))
 
+	// decide what port to use, run on :80 if not set
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "80"
+	}
+	port = ":"+port
+
 	// start the server
-	port := ":5050"
 	log.Printf("Running on port %s", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
