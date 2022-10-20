08433442d5136e33fa0ad6f94641f41e2ae97c69
Ricardo B. Ayres <ricardo.bosqueiro.ayres@gmail.com>
2022-10-19 22:26:41 -0300

    implementando módulo de autorização básica

---
+++ auth.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"crypto/rand"
+	"errors"
+	"crypto/sha256"
+)
+
+const Size = sha256.Size
+
+type Credentials struct {
+	hash [Size]byte
+	salt [Size]byte
+}
+
+func genSalt() ([Size]byte, error) {
+	var salt [Size]byte
+	var err error
+
+	n, err := rand.Read(salt[:])
+	if err != nil {
+		return salt, err
+	}
+
+	if n != Size {
+		err = errors.New("Salt generated with wrong size.")
+	}
+	return salt, err
+}
+
+func passwordHash(pwd string, salt [Size]byte) [Size]byte {
+	salted := append([]byte(pwd), salt[:]...)
+	return sha256.Sum256(salted)
+}
+
+func NewPassword(pwd string) (Credentials, error) {
+	var creds Credentials
+	var err error
+
+	creds.salt, err = genSalt()
+	creds.hash = passwordHash(pwd, creds.salt)
+	return creds, err
+}
+
+func CheckPassword(pwd string, creds Credentials) bool {
+	newhash := passwordHash(pwd, creds.salt)
+	return newhash == creds.hash
+}
